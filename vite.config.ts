45ff14f5ee8934b061105cd6bfd6a8c9ac4a1ce5
Aine-dickson <ainedixon01@gmail.com>
2025-09-02 17:12:33 +0300

    Fix bug in dynamic import

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,19 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: '/',
-    plugins: [
-        vue(),
-        vueDevTools(),
-        tailwindcss()
-    ],
-    resolve: {
-        alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-        },
-    },
+export default defineConfig(({ mode }) => {
+    const isDev = mode === 'development'
+    return {
+        base: '/',
+        plugins: [
+            vue(),
+            isDev && vueDevTools(), // exclude devtools from production build
+            tailwindcss()
+        ].filter(Boolean),
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        }
+    }
 })
